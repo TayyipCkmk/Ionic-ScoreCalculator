47d5c4cefff6c137e952a5d856d7744451c5a157
Mehmet Tayyip Çakmak <tayyip.cakmak@detaysoft.com>
2022-02-02 15:52:54 +0300

    first commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -8,11 +8,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
-
+  public tur:number=0;
+  public mat:number=0;
+  public sos:number=0;
+  public fen:number=0;
+  public sonuc:number=0;
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    
   }
+hesapla(tur,mat,sos,fen) {
+  console.log("hi");
+this.sonuc = 200 + tur*2.3 + mat*2.2 + sos*1.5 + fen*1.5;
+}
 
 }
